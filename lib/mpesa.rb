7376d83a92c172c326fdfa037b73519920e60bf5
gathuku <mosesgathuku95@gmail.com>
2019-11-04 11:36:05 +0300

    Add config block

--- lib/mpesa.rb
+++ lib/mpesa.rb
@@ -1,6 +1,22 @@
 require "mpesa/version"
+require "mpesa/configuration"
 
 module Mpesa
   class Error < StandardError; end
   # Your code goes here...
+class << self
+  attr_accessor :configuration
+end
+
+def self.configuration
+  @configuration ||= Configuration.new
+end
+
+def self.reset
+  @configuration = Configuration.new
+end
+
+def self.configure
+  yield(configuration)
+end
 end
---
+++ lib/mpesa/configuration.rb
@@ -0,0 +1,15 @@
+require "lib/mpesa"
+module Mpesa
+  class Configuration
+    attr_accessor :mpesa_env,:consumer_key, :consumer_secret, :paybil,
+                   :lipa_na_mpesa
+
+      def initialize
+        @mpesa_env=nil
+        @consumer_key=nil
+        @consumer_secret=nil
+        @paybil=nil
+      end
+
+  end
+end
