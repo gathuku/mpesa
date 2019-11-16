13de719bcc80eefbdf56fdfc898fcca656a0a41c
gathuku <mosesgathuku95@gmail.com>
2019-11-16 12:12:42 +0300

    Update

--- test/mpesa_test.rb
+++ test/mpesa_test.rb
@@ -1,14 +1,6 @@
 require "test_helper"
 
 class MpesaTest < Minitest::Test
-def setup
-  @data={
-    'consumer_key':'aR7R09zePq0OSfOttvuQDrfdM4n37i0C',
-    'consumer_secret':'F9AebI6azDlRjLiR',
-    'confirmation_url':'https://524317db.ngrok.io/confirm',
-    'validation_url': 'https://524317db.ngrok.io/validate'
-  }
-end
 
   def test_that_it_has_a_version_number
     refute_nil ::Mpesa::VERSION
@@ -25,5 +17,19 @@ end
 
 
       assert_equal(200, Mpesa.access_key.status)
+  end
+
+  def test_it_registers_urls
+  stub_request(:post, "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl").
+   with(
+     body: {"ConfirmationUrl"=>"https://524317db.ngrok.io/mpesa", "ResponseType"=>"Completed", "ValidationUrl"=>"https://524317db.ngrok.io/mpesa"},
+     headers: {
+ 	  'Accept'=>'application/json',
+ 	  'Authorization'=>'Bearer #{get_access_token}',
+ 	  'Content-Type'=>'application/json',
+     }).
+   to_return(status: 200, body: "", headers: {})
+
+    assert_equal(200, Mpesa.register_urls.status)
   end
 end
