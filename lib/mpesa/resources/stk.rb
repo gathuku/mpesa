e77ef037dde749aad90443d763b782dc58708430
Moses Gathuku <mosesgathuku95@gmail.com>
2022-01-06 18:41:13 +0300

    Fix Wrong credentials issues (#16)
    * fix wrong credentials error when shortcode is passed in client
    
    * give passed pass_key precedence
    
    * update console setup
    
    * fix typo

--- lib/mpesa/resources/stk.rb
+++ lib/mpesa/resources/stk.rb
@@ -12,13 +12,13 @@ module Mpesa
 
     def body
       {
-        'BusinessShortCode': args[:shortcode] || client.shortcode,
+        'BusinessShortCode': shortcode,
         'Password': password,
         'Timestamp': timestamp.to_s,
         'TransactionType': 'CustomerPayBillOnline',
         'Amount': args[:amount],
         'PartyA': args[:phone],
-        'PartyB': args[:shortcode] || client.shortcode,
+        'PartyB': shortcode,
         'PhoneNumber': args[:phone],
         'CallBackURL': args[:callback_url],
         'AccountReference': args[:reference],
@@ -27,11 +27,15 @@ module Mpesa
     end
 
     def password
-      Base64.strict_encode64("#{args[:shortcode]}#{client.pass_key || args[:pass_key]}#{timestamp}")
+      Base64.strict_encode64("#{shortcode}#{args[:pass_key] || client.pass_key}#{timestamp}")
     end
 
     def timestamp
       Time.now.strftime('%Y%m%d%H%M%S').to_i
+    end
+
+    def shortcode
+      args[:shortcode] || client.shortcode
     end
   end
 end
